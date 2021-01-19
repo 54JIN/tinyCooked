05fb8db7c9e3fc7dc1fbcd5731b07212449f594f
54JIN <72236983+54JIN@users.noreply.github.com>
2021-01-18 21:55:03 -0500

    Add files via upload

---
+++ Food.java
@@ -0,0 +1,56 @@
+public class Food{
+    Food head;
+    String foodName;
+    double price;
+    int calories;
+    Food next;
+    //For creating an empty Food class
+    public Food(){
+        this.next = null;
+    }
+    //Declares a food class without calories already declared.
+    public Food(String foodName, double price){
+        this.foodName = foodName;
+        this.price = price;
+        this.next = null;
+    }
+    //Declares a Food class with calories already declared.
+    public Food(String foodName, double price, int calories){
+        this.foodName = foodName;
+        this.price = price;
+        this.calories = calories;
+        this.next = null;
+    }
+    //add a food item without calories being declared
+    public void add(String foodName, double price){
+        Food tempFood = new Food(foodName,price);
+        if(head == null){
+            head = tempFood;
+        }
+        else{
+            Food cursor = head;
+            while(cursor.next != null){
+                cursor = cursor.next;
+            }
+            cursor.next = tempFood;
+        }
+    }
+    //add a food item with calories attached 
+    public void add(String foodName, double price, int calories){
+        Food tempFood = new Food(foodName,price,calories);
+        if(head == null){
+            head = tempFood;
+        }
+        else{
+            Food cursor = head;
+            while(cursor.next != null){
+                cursor = cursor.next;
+            }
+            cursor.next = tempFood;
+        }
+    }
+
+    public static void main(String[] args){
+        System.out.println("Hi");
+    }
+}
