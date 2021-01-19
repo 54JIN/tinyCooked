1987b4b87b77a4c9c0130e92e2a5371982bfbaaa
54JIN <72236983+54JIN@users.noreply.github.com>
2021-01-18 22:31:49 -0500

    Update Food.java

--- Food.java
+++ Food.java
@@ -3,51 +3,19 @@ public class Food{
     String foodName;
     double price;
     int calories;
-    Food next;
-    //For creating an empty Food class
+    Food previous;
+
     public Food(){
-        this.next = null;
+        this.head = null;
+        this.previous = null;
     }
-    //Declares a food class without calories already declared.
     public Food(String foodName, double price){
+        this.previous = null;
         this.foodName = foodName;
         this.price = price;
-        this.next = null;
     }
-    //Declares a Food class with calories already declared.
-    public Food(String foodName, double price, int calories){
-        this.foodName = foodName;
-        this.price = price;
-        this.calories = calories;
-        this.next = null;
-    }
-    //add a food item without calories being declared
-    public void add(String foodName, double price){
-        Food tempFood = new Food(foodName,price);
-        if(head == null){
-            head = tempFood;
-        }
-        else{
-            Food cursor = head;
-            while(cursor.next != null){
-                cursor = cursor.next;
-            }
-            cursor.next = tempFood;
-        }
-    }
-    //add a food item with calories attached 
-    public void add(String foodName, double price, int calories){
-        Food tempFood = new Food(foodName,price,calories);
-        if(head == null){
-            head = tempFood;
-        }
-        else{
-            Food cursor = head;
-            while(cursor.next != null){
-                cursor = cursor.next;
-            }
-            cursor.next = tempFood;
-        }
+    public void add(){
+        
     }
 
     //why u need main
