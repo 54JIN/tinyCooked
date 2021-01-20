b08805ea907e7e016f2835960a6de19d6384517f
54JIN <72236983+54JIN@users.noreply.github.com>
2021-01-20 01:30:25 -0500

    Add files via upload

---
+++ customerInfo.java
@@ -0,0 +1,17 @@
+public class customerInfo{
+    //LinkedList based on a stack list
+    String customerName;
+    String address;
+    String city;
+    String state;
+    int zipCode;
+    int phoneNumber;
+    Food food;
+    Drinks drinks;
+
+    customerInfo head;
+    customerInfo next;
+    public static void main(String[] args){
+        System.out.println("hi");
+    }
+}
