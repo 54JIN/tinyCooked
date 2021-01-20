0c6671af02d2e985acf429f62a5a8607199ea278
54JIN <72236983+54JIN@users.noreply.github.com>
2021-01-20 01:39:07 -0500

    Update customerInfo.java

--- customerInfo.java
+++ customerInfo.java
@@ -10,7 +10,31 @@ public class customerInfo{
     Drinks drinks;
 
     customerInfo head;
-    customerInfo next;
+    customerInfo previous;
+
+    public customerInfo(){
+        this.head = null;
+        this.previous = null;
+    }
+    public customerInfo(String customerName, String address, String city, String state, int zipCode, int phoneNumber){
+        this.customerName = customerName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.phoneNumber = phoneNumber;
+        this.previous = null;
+    }
+    public void add(String customerName, String address, String city, String state, int zipCode, int phoneNumber){
+        customerInfo tempCustomer = new customerInfo(customerName,address,city,state,zipCode,phoneNumber);
+        if(head == null){
+            head = tempCustomer;
+        }
+        else{
+            tempCustomer.previous = head;
+            head = tempCustomer;
+        }
+    }
     public static void main(String[] args){
         System.out.println("hi");
     }
